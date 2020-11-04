a24d77b9939a48f6eca6e2f361e93aacf5639229
Prabhurbvn <prabhurbvn@gmail.com>
2020-11-04 14:34:54 +0530

    Create Sample1.java

---
+++ src/Sample1.java
@@ -0,0 +1,12 @@
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi");
+    
+    System.out.println("Welcome");
+
+	}
+
+}
